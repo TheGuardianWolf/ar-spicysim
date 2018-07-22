63d36adeb71e40b1b5e7a871cfefc1233d8f89bd
Jerry <nano@pixelcollider.net>
2018-07-23 00:32:16 +1200

    Unity and .net standard use

---
+++ ar-spicybread/Assets/VirtualComponent/Placer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VirtualComponent 
+{
+    public abstract class Placer
+    {
+        private ComponentTool activeTool;
+
+        public ComponentTool ActiveTool
+        {
+            get
+            {
+                return activeTool;
+            }
+
+            set
+            {
+
+                activeTool = value;
+            }
+        }
+    }
+}
+
